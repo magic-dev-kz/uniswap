10cb9be3c140343b954d9e530bd9b0fbeff7cea4
AImagic <ai_magic@MacBook-Air-Ila.local>
2026-03-30 09:49:03 +0700

    v25: add security meta tags (nosniff, no-referrer, format-detection)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uniswap-v24';
+const CACHE = 'uniswap-v25';
 const ASSETS = ['./', './index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
